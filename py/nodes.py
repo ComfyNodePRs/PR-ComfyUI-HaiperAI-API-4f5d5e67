6d946dcb6f638803770c57857f8a04dcd879ec92
Tuo He <tuo.he@haiper.ai>
2024-12-05 20:35:39 +0000

    delete imgbb node

--- py/nodes.py
+++ py/nodes.py
@@ -4,11 +4,9 @@ from .haiper_api_node import (
     T2IPipelineNode,
     KFCPipelineNode,
 )
-from .imgbb_node import ImgBBUpload
 
 # A dictionary that contains all nodes you want to export with their names
 NODE_CLASS_MAPPINGS = {
-    "ImgBBUpload": ImgBBUpload,
     "HaiperImage2Video": I2VPipelineNode,
     "HaiperText2Video": T2VPipelineNode,
     "HaiperText2Image": T2IPipelineNode,
@@ -17,7 +15,6 @@ NODE_CLASS_MAPPINGS = {
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "ImgBBUpload": "ImgBB Upload",
     "HaiperImage2Video": "Haiper Image to Video",
     "HaiperText2Video": "Haiper Text to Video",
     "HaiperText2Image": "Haiper Text to Image",
